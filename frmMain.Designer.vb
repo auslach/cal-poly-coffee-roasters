cc83cbae061522e18634bd4d57c3ca13e0e37885
Austin L. Chang <austinlchang@gmail.com>
2015-01-31 02:04:38 -0800

    initial commit

---
+++ frmMain.Designer.vb
@@ -0,0 +1,183 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip = New System.Windows.Forms.MenuStrip()
+        Me.mnuFile = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuFileNew = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuFileClose = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuFileExit = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuEdit = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuEditCut = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuEditCopy = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuEditPaste = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuWindow = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuWindowCascade = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuWindowTileVertical = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuWindowTileHorizontal = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuWindowArrangeIcons = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ToolStripSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.MenuStrip.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip
+        '
+        Me.MenuStrip.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.MenuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuFile, Me.mnuEdit, Me.mnuWindow})
+        Me.MenuStrip.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip.MdiWindowListItem = Me.mnuWindow
+        Me.MenuStrip.Name = "MenuStrip"
+        Me.MenuStrip.Size = New System.Drawing.Size(913, 24)
+        Me.MenuStrip.TabIndex = 2
+        Me.MenuStrip.Text = "MenuStrip"
+        '
+        'mnuFile
+        '
+        Me.mnuFile.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuFileNew, Me.mnuFileClose, Me.ToolStripSeparator1, Me.mnuFileExit})
+        Me.mnuFile.Name = "mnuFile"
+        Me.mnuFile.Size = New System.Drawing.Size(37, 20)
+        Me.mnuFile.Text = "&File"
+        '
+        'mnuFileNew
+        '
+        Me.mnuFileNew.Name = "mnuFileNew"
+        Me.mnuFileNew.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.N), System.Windows.Forms.Keys)
+        Me.mnuFileNew.Size = New System.Drawing.Size(152, 22)
+        Me.mnuFileNew.Text = "New"
+        '
+        'mnuFileClose
+        '
+        Me.mnuFileClose.Name = "mnuFileClose"
+        Me.mnuFileClose.Size = New System.Drawing.Size(152, 22)
+        Me.mnuFileClose.Text = "Close"
+        '
+        'mnuFileExit
+        '
+        Me.mnuFileExit.Name = "mnuFileExit"
+        Me.mnuFileExit.Size = New System.Drawing.Size(152, 22)
+        Me.mnuFileExit.Text = "Exit"
+        '
+        'mnuEdit
+        '
+        Me.mnuEdit.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuEditCut, Me.mnuEditCopy, Me.mnuEditPaste})
+        Me.mnuEdit.Name = "mnuEdit"
+        Me.mnuEdit.Size = New System.Drawing.Size(39, 20)
+        Me.mnuEdit.Text = "&Edit"
+        '
+        'mnuEditCut
+        '
+        Me.mnuEditCut.Name = "mnuEditCut"
+        Me.mnuEditCut.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.X), System.Windows.Forms.Keys)
+        Me.mnuEditCut.Size = New System.Drawing.Size(152, 22)
+        Me.mnuEditCut.Text = "Cu&t"
+        '
+        'mnuEditCopy
+        '
+        Me.mnuEditCopy.Name = "mnuEditCopy"
+        Me.mnuEditCopy.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.C), System.Windows.Forms.Keys)
+        Me.mnuEditCopy.Size = New System.Drawing.Size(152, 22)
+        Me.mnuEditCopy.Text = "Copy"
+        '
+        'mnuEditPaste
+        '
+        Me.mnuEditPaste.Name = "mnuEditPaste"
+        Me.mnuEditPaste.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.V), System.Windows.Forms.Keys)
+        Me.mnuEditPaste.Size = New System.Drawing.Size(152, 22)
+        Me.mnuEditPaste.Text = "Paste"
+        '
+        'mnuWindow
+        '
+        Me.mnuWindow.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuWindowCascade, Me.mnuWindowTileVertical, Me.mnuWindowTileHorizontal, Me.mnuWindowArrangeIcons, Me.ToolStripSeparator2})
+        Me.mnuWindow.Name = "mnuWindow"
+        Me.mnuWindow.Size = New System.Drawing.Size(63, 20)
+        Me.mnuWindow.Text = "&Window"
+        '
+        'mnuWindowCascade
+        '
+        Me.mnuWindowCascade.Name = "mnuWindowCascade"
+        Me.mnuWindowCascade.Size = New System.Drawing.Size(152, 22)
+        Me.mnuWindowCascade.Text = "Cascade"
+        '
+        'mnuWindowTileVertical
+        '
+        Me.mnuWindowTileVertical.Name = "mnuWindowTileVertical"
+        Me.mnuWindowTileVertical.Size = New System.Drawing.Size(152, 22)
+        Me.mnuWindowTileVertical.Text = "Tile Vertical"
+        '
+        'mnuWindowTileHorizontal
+        '
+        Me.mnuWindowTileHorizontal.Name = "mnuWindowTileHorizontal"
+        Me.mnuWindowTileHorizontal.Size = New System.Drawing.Size(152, 22)
+        Me.mnuWindowTileHorizontal.Text = "Tile Horizontal"
+        '
+        'mnuWindowArrangeIcons
+        '
+        Me.mnuWindowArrangeIcons.Name = "mnuWindowArrangeIcons"
+        Me.mnuWindowArrangeIcons.Size = New System.Drawing.Size(152, 22)
+        Me.mnuWindowArrangeIcons.Text = "Arrange Icons"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(149, 6)
+        '
+        'ToolStripSeparator2
+        '
+        Me.ToolStripSeparator2.Name = "ToolStripSeparator2"
+        Me.ToolStripSeparator2.Size = New System.Drawing.Size(149, 6)
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(913, 632)
+        Me.Controls.Add(Me.MenuStrip)
+        Me.IsMdiContainer = True
+        Me.MainMenuStrip = Me.MenuStrip
+        Me.Name = "frmMain"
+        Me.Text = "Cal Poly Coffee Roasters"
+        Me.MenuStrip.ResumeLayout(False)
+        Me.MenuStrip.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip As System.Windows.Forms.MenuStrip
+    Friend WithEvents mnuFile As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuFileNew As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuFileClose As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuFileExit As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuEdit As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuEditCut As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuEditCopy As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuEditPaste As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuWindow As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuWindowCascade As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuWindowTileVertical As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuWindowTileHorizontal As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuWindowArrangeIcons As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator1 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents ToolStripSeparator2 As System.Windows.Forms.ToolStripSeparator
+
+End Class
---
+++ frmMain.vb
@@ -0,0 +1,111 @@
+﻿Public Class frmMain
+
+    Private m_ChildFormNumber As Integer = 0
+
+    Private Sub mnuFile_DropDownOpening(sender As Object, e As EventArgs) Handles mnuFile.DropDownOpening
+        If Me.ActiveMdiChild IsNot Nothing Then
+            mnuFileClose.Enabled = True
+        Else
+            mnuFileClose.Enabled = False
+        End If
+    End Sub
+
+    Private Sub mnuFileNew_Click(sender As Object, e As EventArgs) Handles mnuFileNew.Click
+        ' New instance of the order form
+        Dim frm As New frmOrder
+        frm.MdiParent = Me
+        m_ChildFormNumber += 1
+        frm.Text = "Order #" & m_ChildFormNumber
+        frm.Show()
+    End Sub
+
+    Private Sub mnuFileClose_Click(sender As Object, e As EventArgs) Handles mnuFileClose.Click
+        ' Close the active order form
+        Me.ActiveMdiChild.Close()
+    End Sub
+
+    Private Sub mnuFileExit_Click(sender As Object, e As EventArgs) Handles mnuFileExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub mnuWindowCascade_Click(sender As Object, e As EventArgs) Handles mnuWindowCascade.Click
+        Me.LayoutMdi(MdiLayout.Cascade)
+    End Sub
+
+    Private Sub mnuWindowTileVertical_Click(sender As Object, e As EventArgs) Handles mnuWindowTileVertical.Click
+        Me.LayoutMdi(MdiLayout.TileVertical)
+    End Sub
+
+    Private Sub mnuWindowTileHorizontal_Click(sender As Object, e As EventArgs) Handles mnuWindowTileHorizontal.Click
+        Me.LayoutMdi(MdiLayout.TileHorizontal)
+    End Sub
+
+    Private Sub mnuWindowArrangeIcons_Click(sender As Object, e As EventArgs) Handles mnuWindowArrangeIcons.Click
+        Me.LayoutMdi(MdiLayout.ArrangeIcons)
+    End Sub
+
+    Public Sub CheckEditMenu()
+        mnuEditCut.Enabled = False
+        mnuEditCopy.Enabled = False
+        mnuEditPaste.Enabled = False
+
+        If Not ActiveMdiChild Is Nothing Then
+            If Not ActiveMdiChild.ActiveControl Is Nothing Then
+                ' check if active control is text
+                ' if active control is textbox
+                If TypeOf ActiveMdiChild.ActiveControl Is TextBox Then
+                    If CType(ActiveMdiChild.ActiveControl, TextBox).SelectedText <> "" Then
+                        mnuEditCut.Enabled = True
+                        mnuEditCopy.Enabled = True
+                    End If
+                End If
+
+                ' if clipboard contains text & active control is textbox
+                If TypeOf ActiveMdiChild.ActiveControl Is TextBox Then
+                    If Clipboard.ContainsText() Then
+                        mnuEditPaste.Enabled = True
+                    End If
+                End If
+            End If
+        End If
+    End Sub
+
+    Private Sub mnuEditCut_Click(sender As Object, e As EventArgs) Handles mnuEditCut.Click
+        CheckEditMenu()
+        If mnuEditCut.Enabled = True Then
+            ' Cut text, add to clipboard
+            ' Check for textbox
+            If TypeOf Me.ActiveMdiChild.ActiveControl Is TextBox Then
+                Clipboard.SetText(CType(Me.ActiveMdiChild.ActiveControl, TextBox).SelectedText())
+                CType(Me.ActiveMdiChild.ActiveControl, TextBox).SelectedText = ""
+            End If
+        End If
+    End Sub
+
+    Private Sub mnuEditCopy_Click(sender As Object, e As EventArgs) Handles mnuEditCopy.Click
+        CheckEditMenu()
+        If mnuEditCopy.Enabled = True Then
+            ' copy text, add to clipboard
+            ' check for textbox
+            If TypeOf Me.ActiveMdiChild.ActiveControl Is TextBox Then
+                Clipboard.SetText(CType(Me.ActiveMdiChild.ActiveControl, TextBox).SelectedText())
+            End If
+        End If
+    End Sub
+
+    Private Sub mnuEditPaste_Click(sender As Object, e As EventArgs) Handles mnuEditPaste.Click
+        CheckEditMenu()
+        If mnuEditPaste.Enabled = True Then
+            ' paste text from clipboard to active control
+            ' must be textbox
+            If TypeOf Me.ActiveMdiChild.ActiveControl Is TextBox Then
+                CType(Me.ActiveMdiChild.ActiveControl, TextBox).SelectedText() = Clipboard.GetText()
+            End If
+        End If
+    End Sub
+
+    Private Sub mnuEdit_DropDownOpening(sender As Object, e As EventArgs) Handles mnuEdit.DropDownOpening
+        CheckEditMenu()
+    End Sub
+
+End Class
