27207e2b64cfd6c960d0e0fffe9b1a705ec637a2
Austin L. Chang <austinlchang@gmail.com>
2015-01-31 20:14:01 -0800

    initialize coffee menu on order form load

--- frmOrder.Designer.vb
+++ frmOrder.Designer.vb
@@ -26,12 +26,38 @@ Partial Class frmOrder
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.grpMenu = New System.Windows.Forms.GroupBox()
+        Me.grpOrderSummary = New System.Windows.Forms.GroupBox()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.lblQuantity = New System.Windows.Forms.Label()
+        Me.lblPrice = New System.Windows.Forms.Label()
+        Me.lblItem = New System.Windows.Forms.Label()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.grpOrderTotal = New System.Windows.Forms.GroupBox()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.lblOrderTotal = New System.Windows.Forms.Label()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.lblSalesTax = New System.Windows.Forms.Label()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.lblFoodTotal = New System.Windows.Forms.Label()
+        Me.btnTotal = New System.Windows.Forms.Button()
+        Me.btnNewOrder = New System.Windows.Forms.Button()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.grpOrderSummary.SuspendLayout()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.grpOrderTotal.SuspendLayout()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(256, 9)
+        Me.Label1.Location = New System.Drawing.Point(30, 51)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(46, 13)
         Me.Label1.TabIndex = 0
@@ -40,7 +66,7 @@ Partial Class frmOrder
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(256, 31)
+        Me.Label2.Location = New System.Drawing.Point(35, 82)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(41, 13)
         Me.Label2.TabIndex = 1
@@ -49,7 +75,7 @@ Partial Class frmOrder
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(337, 152)
+        Me.Label3.Location = New System.Drawing.Point(15, 115)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(61, 13)
         Me.Label3.TabIndex = 2
@@ -58,23 +84,240 @@ Partial Class frmOrder
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(67, 30)
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(12, 9)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(39, 13)
+        Me.Label4.Size = New System.Drawing.Size(342, 33)
         Me.Label4.TabIndex = 3
-        Me.Label4.Text = "Label4"
+        Me.Label4.Text = "Cal Poly Coffee Roasters"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(82, 48)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(62, 20)
+        Me.TextBox1.TabIndex = 4
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(82, 79)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(121, 20)
+        Me.TextBox2.TabIndex = 5
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(82, 112)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(121, 20)
+        Me.TextBox3.TabIndex = 6
+        '
+        'grpMenu
+        '
+        Me.grpMenu.Location = New System.Drawing.Point(18, 138)
+        Me.grpMenu.Name = "grpMenu"
+        Me.grpMenu.Size = New System.Drawing.Size(185, 295)
+        Me.grpMenu.TabIndex = 8
+        Me.grpMenu.TabStop = False
+        Me.grpMenu.Text = "Coffee Flavors"
+        '
+        'grpOrderSummary
+        '
+        Me.grpOrderSummary.Controls.Add(Me.lblTotal)
+        Me.grpOrderSummary.Controls.Add(Me.lblQuantity)
+        Me.grpOrderSummary.Controls.Add(Me.lblPrice)
+        Me.grpOrderSummary.Controls.Add(Me.lblItem)
+        Me.grpOrderSummary.Controls.Add(Me.NumericUpDown1)
+        Me.grpOrderSummary.Controls.Add(Me.TextBox5)
+        Me.grpOrderSummary.Controls.Add(Me.TextBox4)
+        Me.grpOrderSummary.Controls.Add(Me.ComboBox1)
+        Me.grpOrderSummary.Location = New System.Drawing.Point(218, 45)
+        Me.grpOrderSummary.Name = "grpOrderSummary"
+        Me.grpOrderSummary.Size = New System.Drawing.Size(389, 263)
+        Me.grpOrderSummary.TabIndex = 9
+        Me.grpOrderSummary.TabStop = False
+        Me.grpOrderSummary.Text = "Order Summary"
+        '
+        'lblTotal
+        '
+        Me.lblTotal.AutoSize = True
+        Me.lblTotal.Location = New System.Drawing.Point(296, 19)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(31, 13)
+        Me.lblTotal.TabIndex = 7
+        Me.lblTotal.Text = "Total"
+        '
+        'lblQuantity
+        '
+        Me.lblQuantity.AutoSize = True
+        Me.lblQuantity.Location = New System.Drawing.Point(233, 19)
+        Me.lblQuantity.Name = "lblQuantity"
+        Me.lblQuantity.Size = New System.Drawing.Size(46, 13)
+        Me.lblQuantity.TabIndex = 6
+        Me.lblQuantity.Text = "Quantity"
+        '
+        'lblPrice
+        '
+        Me.lblPrice.AutoSize = True
+        Me.lblPrice.Location = New System.Drawing.Point(163, 19)
+        Me.lblPrice.Name = "lblPrice"
+        Me.lblPrice.Size = New System.Drawing.Size(31, 13)
+        Me.lblPrice.TabIndex = 5
+        Me.lblPrice.Text = "Price"
+        '
+        'lblItem
+        '
+        Me.lblItem.AutoSize = True
+        Me.lblItem.Location = New System.Drawing.Point(17, 16)
+        Me.lblItem.Name = "lblItem"
+        Me.lblItem.Size = New System.Drawing.Size(27, 13)
+        Me.lblItem.TabIndex = 4
+        Me.lblItem.Text = "Item"
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(233, 34)
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(57, 20)
+        Me.NumericUpDown1.TabIndex = 3
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(296, 34)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(61, 20)
+        Me.TextBox5.TabIndex = 2
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(166, 35)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(61, 20)
+        Me.TextBox4.TabIndex = 1
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(18, 34)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(142, 21)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'grpOrderTotal
+        '
+        Me.grpOrderTotal.Controls.Add(Me.TextBox8)
+        Me.grpOrderTotal.Controls.Add(Me.lblOrderTotal)
+        Me.grpOrderTotal.Controls.Add(Me.TextBox7)
+        Me.grpOrderTotal.Controls.Add(Me.lblSalesTax)
+        Me.grpOrderTotal.Controls.Add(Me.TextBox6)
+        Me.grpOrderTotal.Controls.Add(Me.lblFoodTotal)
+        Me.grpOrderTotal.Location = New System.Drawing.Point(218, 314)
+        Me.grpOrderTotal.Name = "grpOrderTotal"
+        Me.grpOrderTotal.Size = New System.Drawing.Size(204, 119)
+        Me.grpOrderTotal.TabIndex = 10
+        Me.grpOrderTotal.TabStop = False
+        Me.grpOrderTotal.Text = "OrderTotal"
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(81, 90)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(102, 20)
+        Me.TextBox8.TabIndex = 7
+        '
+        'lblOrderTotal
+        '
+        Me.lblOrderTotal.AutoSize = True
+        Me.lblOrderTotal.Location = New System.Drawing.Point(15, 94)
+        Me.lblOrderTotal.Name = "lblOrderTotal"
+        Me.lblOrderTotal.Size = New System.Drawing.Size(63, 13)
+        Me.lblOrderTotal.TabIndex = 6
+        Me.lblOrderTotal.Text = "Order Total:"
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(81, 54)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(102, 20)
+        Me.TextBox7.TabIndex = 5
+        '
+        'lblSalesTax
+        '
+        Me.lblSalesTax.AutoSize = True
+        Me.lblSalesTax.Location = New System.Drawing.Point(21, 57)
+        Me.lblSalesTax.Name = "lblSalesTax"
+        Me.lblSalesTax.Size = New System.Drawing.Size(57, 13)
+        Me.lblSalesTax.TabIndex = 4
+        Me.lblSalesTax.Text = "Sales Tax:"
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(81, 18)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(102, 20)
+        Me.TextBox6.TabIndex = 3
+        '
+        'lblFoodTotal
+        '
+        Me.lblFoodTotal.AutoSize = True
+        Me.lblFoodTotal.Location = New System.Drawing.Point(17, 21)
+        Me.lblFoodTotal.Name = "lblFoodTotal"
+        Me.lblFoodTotal.Size = New System.Drawing.Size(61, 13)
+        Me.lblFoodTotal.TabIndex = 0
+        Me.lblFoodTotal.Text = "Food Total:"
+        '
+        'btnTotal
+        '
+        Me.btnTotal.Location = New System.Drawing.Point(433, 338)
+        Me.btnTotal.Name = "btnTotal"
+        Me.btnTotal.Size = New System.Drawing.Size(75, 23)
+        Me.btnTotal.TabIndex = 11
+        Me.btnTotal.Text = "Total"
+        Me.btnTotal.UseVisualStyleBackColor = True
+        '
+        'btnNewOrder
+        '
+        Me.btnNewOrder.Location = New System.Drawing.Point(433, 367)
+        Me.btnNewOrder.Name = "btnNewOrder"
+        Me.btnNewOrder.Size = New System.Drawing.Size(75, 23)
+        Me.btnNewOrder.TabIndex = 12
+        Me.btnNewOrder.Text = "New Order"
+        Me.btnNewOrder.UseVisualStyleBackColor = True
+        '
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(433, 396)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(75, 23)
+        Me.btnClose.TabIndex = 13
+        Me.btnClose.Text = "Close"
+        Me.btnClose.UseVisualStyleBackColor = True
         '
         'frmOrder
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(662, 445)
+        Me.ClientSize = New System.Drawing.Size(619, 445)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.btnNewOrder)
+        Me.Controls.Add(Me.btnTotal)
+        Me.Controls.Add(Me.grpOrderTotal)
+        Me.Controls.Add(Me.grpOrderSummary)
+        Me.Controls.Add(Me.grpMenu)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Name = "frmOrder"
         Me.Text = "frmOrder"
+        Me.grpOrderSummary.ResumeLayout(False)
+        Me.grpOrderSummary.PerformLayout()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.grpOrderTotal.ResumeLayout(False)
+        Me.grpOrderTotal.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -83,4 +326,27 @@ Partial Class frmOrder
     Friend WithEvents Label2 As System.Windows.Forms.Label
     Friend WithEvents Label3 As System.Windows.Forms.Label
     Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents grpMenu As System.Windows.Forms.GroupBox
+    Friend WithEvents grpOrderSummary As System.Windows.Forms.GroupBox
+    Friend WithEvents NumericUpDown1 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents grpOrderTotal As System.Windows.Forms.GroupBox
+    Friend WithEvents TextBox6 As System.Windows.Forms.TextBox
+    Friend WithEvents lblFoodTotal As System.Windows.Forms.Label
+    Friend WithEvents btnTotal As System.Windows.Forms.Button
+    Friend WithEvents btnNewOrder As System.Windows.Forms.Button
+    Friend WithEvents btnClose As System.Windows.Forms.Button
+    Friend WithEvents TextBox8 As System.Windows.Forms.TextBox
+    Friend WithEvents lblOrderTotal As System.Windows.Forms.Label
+    Friend WithEvents TextBox7 As System.Windows.Forms.TextBox
+    Friend WithEvents lblSalesTax As System.Windows.Forms.Label
+    Friend WithEvents lblTotal As System.Windows.Forms.Label
+    Friend WithEvents lblQuantity As System.Windows.Forms.Label
+    Friend WithEvents lblPrice As System.Windows.Forms.Label
+    Friend WithEvents lblItem As System.Windows.Forms.Label
 End Class
--- frmOrder.vb
+++ frmOrder.vb
@@ -1,3 +1,57 @@
 ﻿Public Class frmOrder
 
+    Private coffeeMenu As New Dictionary(Of String, Double)
+    Private cboDetailLines As New ArrayList
+
+    Private Sub frmOrder_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' add coffees and prices to collection
+        coffeeMenu.Add("Antigua", 5.95)
+        coffeeMenu.Add("Apanas", 11.95)
+        coffeeMenu.Add("Bantu", 9.92)
+        coffeeMenu.Add("Colombia", 11.52)
+        coffeeMenu.Add("Costa Rica", 13.6)
+        coffeeMenu.Add("Ethiopia", 9.51)
+        coffeeMenu.Add("French Roast", 10.72)
+        coffeeMenu.Add("Huehuetenango", 10.95)
+        coffeeMenu.Add("Kenya", 13.6)
+        coffeeMenu.Add("Mexico", 11.52)
+        coffeeMenu.Add("Morning Ed.", 9.92)
+        coffeeMenu.Add("Nepenthe", 11.52)
+        coffeeMenu.Add("Sumatra", 9.92)
+        coffeeMenu.Add("Yemen", 8.96)
+        coffeeMenu.Add("Yemen Mocha", 18.52)
+        coffeeMenu.Add("Zimbabwe", 11.52)
+
+        Dim positionFromTop As Integer = 0
+
+        Dim grpMenuLines As New ArrayList
+        For Each coffee As Object In coffeeMenu
+            Dim mnuItemName As New Label
+            Dim mnuItemPrice As New Label
+            positionFromTop += 17
+
+            mnuItemName.AutoSize = True
+            mnuItemName.Left = 10
+            mnuItemName.Text = coffee.key
+            mnuItemName.Top = positionFromTop
+            mnuItemPrice.AutoSize = True
+            mnuItemPrice.TextAlign = ContentAlignment.TopRight
+            mnuItemPrice.Left = 130
+            mnuItemPrice.Text = "$ " & coffee.value
+            mnuItemPrice.Top = positionFromTop
+
+
+            Console.WriteLine(coffee.key)
+            Console.WriteLine(coffee.value.ToString)
+
+            ' add control to the group box Coffee Menu
+            grpMenu.Controls.Add(mnuItemName)
+            grpMenu.Controls.Add(mnuItemPrice)
+
+        Next
+
+
+
+    End Sub
+
 End Class
