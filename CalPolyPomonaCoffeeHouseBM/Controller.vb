9a1e3aa41baf2a33aa13996d56bbc52cce824a64
Austin L. Chang <austinlchang@gmail.com>
2015-03-10 22:21:02 -0700

    rename projects, add db and controller
    rename projects to DB, BM, UI
    add database controller
    add add and remove order functions

--- CalPolyPomonaCoffeeHouseBM/Controller.vb
+++ CalPolyPomonaCoffeeHouseBM/Controller.vb
@@ -1,4 +1,7 @@
-﻿Public Class Controller
+﻿Imports CalPolyPomonaCoffeeHouseDB
+Imports System.Data.SqlServerCe
+
+Public Class Controller
 
     Private OrderCollection As New Collection
     Private menu As New Menu
@@ -32,19 +35,52 @@
     End Function
 
     Public Function addOrder(ByVal myOrder As Order, Optional replace As Boolean = False) As String
+        'Dim msg As String = ""
+        'If myOrder IsNot Nothing Then
+        '    If isValidOrder(myOrder, msg) Then
+        '        OrderCollection.Add(myOrder, myOrder.ID)
+        '    Else
+        '        If replace = True Then
+        '            OrderCollection.Remove(myOrder.ID)
+        '            OrderCollection.Add(myOrder, myOrder.ID)
+        '        End If
+        '    End If
+        'End If
+        'Return msg
         Dim msg As String = ""
+        DBController.Open()
         If myOrder IsNot Nothing Then
             If isValidOrder(myOrder, msg) Then
-                OrderCollection.Add(myOrder, myOrder.ID)
+                'OrderCollection.Add(myOrder, myOrder.ID)
+                ' order is valid, add it to db
+                DBController.addOrder(myOrder.ID, myOrder.server, myOrder.orderDate)
+
+                Dim line As OrderDetail
+                For lineNo As Integer = 0 To myOrder.countItems - 1
+                    line = myOrder.getLine(lineNo)
+                    With line
+                        DBController.addDetail(myOrder.ID, .name, .qty, .price, lineNo + 1)
+                    End With
+                Next
             Else
                 If replace = True Then
-                    OrderCollection.Remove(myOrder.ID)
-                    OrderCollection.Add(myOrder, myOrder.ID)
+                    'OrderCollection.Remove(myOrder.ID)
+                    'OrderCollection.Add(myOrder, myOrder.ID)
+                    delete(myOrder.ID)
+                    addOrder(myOrder)
                 End If
             End If
         End If
+
+        DBController.close()
         Return msg
     End Function
+
+    Public Sub delete(id As String)
+        DBController.open()
+        DBController.deleteOrder(id)
+        DBController.close()
+    End Sub
 
     Public Sub removeOrder(ByVal myOrder As Order)
         If myOrder IsNot Nothing Then
@@ -56,8 +92,8 @@
         Return menu
     End Function
 
-    Public Function getOrder(ByVal ID As String) As Order
-        Return OrderCollection.Item(ID)
+    Public Function getOrder(ByVal id As String) As Order
+        'Return OrderCollection.Item(ID)
     End Function
 
     Public Function getAllOrders() As Collection
@@ -68,7 +104,7 @@
     Private Function isValidOrder(myOrder As Order, ByRef msg As String) As Boolean
         Dim result As Boolean = False
 
-        If Not OrderCollection.Contains(myOrder.ID) Then
+        If Not DBController.isPresent(myOrder.ID) Then
             result = True
         Else
             msg += "Duplicate order found. Replace?"
---
+++ CalPolyPomonaCoffeeHouseDB/DBController.vb
@@ -0,0 +1,69 @@
+﻿Imports System.Data.SqlServerCe
+
+Public Class DBController
+    Private Shared cn As SqlCeConnection
+    Private Shared cnString As String =
+        "Data source = '..\..\..\CalPolyCoffeeHouseDb\Orders.sdf'"
+
+    Private Shared command As SqlCeCommand
+    Private Shared insertCommand As SqlCeCommand
+    Private Shared deleteCommand As SqlCeCommand
+
+    Public Shared Sub open()
+        cn = New SqlCeConnection()
+        cn.ConnectionString = cnString
+        cn.Open()
+    End Sub
+
+    Public Shared Sub close()
+        cn.Close()
+    End Sub
+
+    Public Shared Sub addOrder(id As String, server As String, datetime As String)
+        Dim sql As String
+        sql = "Insert into Orders values ('" & id & "','" & server & "','" & datetime & "')"
+        insertCommand = New SqlCeCommand(sql, cn)
+        insertCommand.ExecuteNonQuery()
+    End Sub
+
+    Public Shared Sub addDetail(id As String, name As String, qty As Double, price As Double, lineNo As Integer)
+        Dim sql As String
+        sql = ""
+        sql += "Insert into OrderDetails values ('"
+        sql += id & "','"
+        sql += qty & "','"
+        sql += price & "','"
+        sql += lineNo & ")"
+
+        insertCommand = New SqlCeCommand(sql, cn)
+        insertCommand.ExecuteNonQuery()
+    End Sub
+
+    Public Shared Sub deleteOrder(id As String)
+        Dim sql As String
+        sql = ""
+        sql += "Delete from Details where order_id = '" & id & "'"
+        deleteCommand = New SqlCeCommand(sql, cn)
+        deleteCommand.ExecuteNonQuery()
+
+        sql = ""
+        sql += "Delete from Orders where id = '" & id & "'"
+        deleteCommand = New SqlCeCommand(sql, cn)
+        deleteCommand.ExecuteNonQuery()
+    End Sub
+
+    'Public Shared Function getOrder(id As String) As SqlCeDataReader
+    'End Function
+
+    Public Shared Function isPresent(id As String) As Boolean
+        Dim exists As Boolean = False
+        Dim sql As String
+        sql = "Select count(*) from Orders where ID = '" & id & "'"
+        command = New SqlCeCommand(sql, cn)
+        If command.ExecuteScalar() > 0 Then
+            exists = True
+        End If
+        Return exists
+    End Function
+
+End Class
